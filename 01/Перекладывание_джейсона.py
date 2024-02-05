1e53f480cdff8e5b3db271a4114499f7e464ea76
neeearina <arishschkarussia@gmail.com>
2024-02-05 12:06:24 +0500

    annotations

--- 01/Перекладывание_джейсона.py
+++ 01/Перекладывание_джейсона.py
@@ -3,6 +3,9 @@ import json
 
 
 class Incidents:
+    """Класс записывает данные из json файла в csv
+    в порядке убывания даты инцидента"""
+
     def __init__(self):
         self.__headers = ["creature", "place", "danger", "date"]
         self.__result = []
@@ -12,6 +15,8 @@ class Incidents:
         self.__write_to_csv()
 
     def __result_data(self):
+        """Функция выбирает определенные заголовик из json и
+        сортирует итоговый список по убыванию даты"""
         with open("cases.json", "r") as json_file:
             json_data = json.load(json_file)
         for d in json_data:
@@ -21,6 +26,7 @@ class Incidents:
         self.__result.sort(key=lambda item: item[3], reverse=True)
 
     def __write_to_csv(self):
+        """Функция записывает итоговый список в csv файл"""
         with open("incidents.csv", "w") as csv_file:
             csvwriter = csv.writer(csv_file)
             csvwriter.writerow(self.__headers)
