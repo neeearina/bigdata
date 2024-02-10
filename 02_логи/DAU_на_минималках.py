d5023bb9fb75a3de8e121ca1d1263c2fb6557b89
neeearina <arishschkarussia@gmail.com>
2024-02-10 10:11:36 +0500

    first task 02

---
+++ 02_логи/DAU_на_минималках.py
@@ -0,0 +1,12 @@
+import typing
+
+data: dict[str, typing.Union[set, int]] = {}
+
+with open("logs.tsv", "r") as file:
+    """Создаем словарь с данными о логах"""
+    for log in file.readlines()[1:]:
+        data_log = log.split()
+        data.setdefault(data_log[1][:10], set()).add(data_log[0])
+
+for k in sorted(data):
+    print(f"{k}: {len(data[k])}")
