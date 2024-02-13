ef3cd292a62e34b2b459de9bab69279d38f9b039
neeearina <arishschkarussia@gmail.com>
2024-02-13 15:53:22 +0500

    simple mapper with list

--- lectures/map_reduce.py
+++ lectures/map_reduce.py
@@ -6,15 +6,28 @@ class Number:
     value: int
 
 
+def digit_mapper(num: Number):
+    if 10 <= num.value <= 99:
+        return (
+            Number(num.value % 10),
+            Number(num.value // 10 % 10)
+        )
+    else:
+        return []
+
+
+def flatten(stream):
+    result = []
+    for it in stream:
+        for element in it:
+            result.append(element)
+    return result
+
+
 input_stream = [
     Number(9), Number(34),
     Number(90), Number(46),
     Number(120), Number(11),
 ]
-output_stream = []
-for item in input_stream:
-    if 10 <= item.value <= 99:
-        output_stream.append(Number(item.value % 10))
-        output_stream.append(Number(item.value // 10 % 10))
-
-print(output_stream)
+output_stream = flatten(map(digit_mapper, input_stream))
+print(list(output_stream))
